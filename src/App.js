a91765dd383207a00c152158bc851f3e5a184b8d
David Foster <dlfosterii@yahoo.com>
2020-06-15 12:16:52 -0400

    exercise complete

--- src/App.js
+++ src/App.js
@@ -32,22 +32,57 @@ class App extends React.Component {
     )
   }
 
+  onComplete = (index) => {
+    let newTodos = [...this.state.todos] //makes a separate copy of the array
+    newTodos[index].complete = true
+    this.setState({
+      todos: newTodos
+    }
+    )
+  }
+
+  onRevert = (index) => {
+    let curTodos = [...this.state.todos] //makes a separate copy of the array
+    curTodos[index].complete = false
+    this.setState({
+      todos: curTodos
+    }
+    )
+  }
+
+  onDelete = (index) => {
+    let curTodos = [...this.state.todos] //makes a separate copy of the array
+      curTodos.splice(index, 1);
+      this.setState({todos: curTodos})
+  }
+
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-            <h1>Todo Form App</h1>
-
+          <h1>Todo Form App</h1>
         </header>
-        <TodoForm onFormSubmit={this.onFormSubmit}/>
-        <ul>
-          {this.state.todos.map((todo, index) => {
+        <TodoForm onFormSubmit={this.onFormSubmit} />
+
+        <div>
+          <h1>Task List</h1>
+        </div>
+        {this.state.todos.map((todo, index) => {
+          if (todo.complete == false) {
             return (
-            <li key={index}>{todo.task} ({todo.complete ? 'done' : 'not done'})<button>Remove</button></li>
+              <li key={index}> {todo.task} <button onClick={() => this.onComplete(index)}>Complete</button> <button onClick={()=>this.onDelete(index)}>Delete</button></li>
             )
-          })}
-        </ul>
+          }
+        })}
+
+        <h1>Completed Tasks</h1>
+        {this.state.todos.map((todo, index) => {
+          if (todo.complete)
+            return (
+              <li style={{ textDecoration: 'line-through' }} key={index}> {todo.task} <button onClick={() => this.onRevert(index)}>Not Done</button> <button onClick={()=>this.onDelete(index)}>Delete</button></li>
+            )
+        })}
       </div>
     )
 
